58da60f98ae3ad5c6676d2f388d553a3871e8dd5
VihangaDehigaspitiya <rashmikarc@gmail.com>
2021-11-15 17:57:37 +0530

    1. react router dom version change to v5.2 2. home and search files added

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,20 @@
 import LoginNRegister from "./pages/LoginNRegister";
+import Home from "./pages/home";
+import {
+    BrowserRouter,
+    Route,
+    Switch
+} from 'react-router-dom';
 
 function App() {
     return (
         <div className="App">
-            <LoginNRegister/>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" exact component={Home}/>
+                    <Route path="/login" component={LoginNRegister}/>
+                </Switch>
+            </BrowserRouter>
         </div>
     );
 }
